0a31edc5a1711241117eff0ee4098e44b9259492
hanyehoon <tlrtjr@naver.com>
2019-04-05 13:12:11 +0900

    에러 수정

--- src/main/java/CTIP/App.java
+++ src/main/java/CTIP/App.java
@@ -10,7 +10,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        printf("Hi");
-        printf("Hello!!");
+        //printf("Hi");
+        //printf("Hello!!");
     }
 }
