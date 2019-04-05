d6df43b5761e8545b9df52082571577f62054927
aeomhs <t_aeom@naver.com>
2019-04-05 12:41:55 +0900

    Junit4 -> Junit5

--- src/test/java/CTIP/AppTest.java
+++ src/test/java/CTIP/AppTest.java
@@ -3,12 +3,15 @@
  */
 package CTIP;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
+    @Test
+    public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
 }
+
