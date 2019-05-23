f99c3b7ff0b7366eba9d3d8eb0d697421a6369b7
biccon <admin@idx.kr>
2019-05-23 20:29:16 +0900

    o ta

--- src/main/java/GUI/WatchButton.java
+++ src/main/java/GUI/WatchButton.java
@@ -85,7 +85,7 @@ public class WatchButton extends JLabel {
                 //System.out.println("Hold");
             } else {
                 if(t.eventClick != null){
-                    String commandNagime = "Click";
+                    String commandName = "Click";
                     ActionEvent event = new ActionEvent(this, uniqueId, commandName);
                     t.eventClick.actionPerformed(event);
                 }
